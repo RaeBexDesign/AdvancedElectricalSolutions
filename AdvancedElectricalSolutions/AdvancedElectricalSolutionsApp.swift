4ef49904c59fa8860cc57716d9d4c85efe5e63e8
Josias Ballard <josiasballard@icloud.com>
2022-02-10 11:34:34 -0500

    Initial Commit

---
+++ AdvancedElectricalSolutions/AdvancedElectricalSolutionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedElectricalSolutionsApp.swift
+//  AdvancedElectricalSolutions
+//
+//  Created by Josias Ballard on 2/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedElectricalSolutionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
